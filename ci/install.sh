59a3aa19cd39737ae803d679f2ed915c36352725
Holger Rapp <hrapp@google.com>
2017-04-27 16:08:41 +0200

    Fix travis.

--- ci/install.sh
+++ ci/install.sh
@@ -6,36 +6,6 @@ set -ex
 
 . $(dirname $0)/utils.sh
 
-install_rustup() {
-    # uninstall the rust toolchain installed by travis, we are going to use rustup
-    sh ~/rust/lib/rustlib/uninstall.sh
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
-    rustc -V
-    cargo -V
-}
-
-install_standard_crates() {
-    if [ $(host) != "$TARGET" ]; then
-        rustup target add $TARGET
-    fi
-}
-
-configure_cargo() {
-    local prefix=$(gcc_prefix)
-
-    if [ ! -z $prefix ]; then
-        # information about the cross compiler
-        ${prefix}gcc -v
-
-        # tell cargo which linker to use for cross compilation
-        mkdir -p .cargo
-        cat >>.cargo/config <<EOF
-[target.$TARGET]
-linker = "${prefix}gcc"
-EOF
-    fi
-}
-
 install_javascript_stuff() {
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
   source ~/.nvm/nvm.sh
@@ -49,9 +19,6 @@ install_javascript_stuff() {
 
 main() {
     install_javascript_stuff
-    install_rustup
-    install_standard_crates
-    configure_cargo
 }
 
 main
