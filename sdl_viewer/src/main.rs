843047023f8a31f42237fd023ceccd6ce89626b5
Holger Rapp <HolgerRapp@gmx.net>
2017-06-02 16:05:57 +0200

    Use level_of_detail when drawing nodes. (#34)

--- sdl_viewer/src/main.rs
+++ sdl_viewer/src/main.rs
@@ -14,6 +14,7 @@
 
 extern crate cgmath;
 extern crate point_viewer;
+extern crate rand;
 extern crate sdl2;
 extern crate time;
 #[macro_use]
@@ -23,6 +24,7 @@ extern crate clap;
 use cgmath::{Array, Matrix, Matrix4};
 use point_viewer::math::CuboidLike;
 use point_viewer::octree;
+use rand::{Rng, thread_rng};
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Scancode;
 use sdl2::video::GLProfile;
@@ -37,6 +39,17 @@ use std::str;
 
 const FRAGMENT_SHADER: &'static str = include_str!("../shaders/points.fs");
 const VERTEX_SHADER: &'static str = include_str!("../shaders/points.vs");
+
+fn reshuffle(new_order: &[usize], old_data: Vec<u8>, bytes_per_vertex: usize) -> Vec<u8> {
+    assert_eq!(new_order.len() * bytes_per_vertex, old_data.len());
+    let mut new_data = Vec::with_capacity(old_data.len());
+    for point_index in new_order {
+        let i = point_index * bytes_per_vertex;
+        new_data.extend(&old_data[i..i + bytes_per_vertex]);
+    }
+    assert_eq!(old_data.len(), new_data.len());
+    new_data
+}
 
 struct NodeDrawer {
     program: GlProgram,
@@ -76,17 +89,20 @@ impl NodeDrawer {
         }
     }
 
-    fn draw(&self, node_view: &NodeView) -> i64 {
+    fn draw(&self, node_view: &NodeView, level_of_detail: i32) -> i64 {
         node_view.vertex_array.bind();
+        let num_points = node_view
+            .meta
+            .num_points_for_level_of_detail(level_of_detail);
         unsafe {
             gl::Uniform1f(
                 self.u_edge_length,
                 node_view.meta.bounding_cube.edge_length(),
             );
             gl::Uniform3fv(self.u_min, 1, node_view.meta.bounding_cube.min().as_ptr());
-            gl::DrawArrays(gl::POINTS, 0, node_view.meta.num_points as i32);
+            gl::DrawArrays(gl::POINTS, 0, num_points as i32);
         }
-        node_view.meta.num_points
+        num_points
     }
 }
 
@@ -105,6 +121,23 @@ impl NodeView {
         let vertex_array = GlVertexArray::new();
         vertex_array.bind();
 
+        // We draw the points in random order. This allows us to only draw the first N if we want
+        // to draw less.
+        let mut indices: Vec<usize> = (0..node_data.meta.num_points as usize).collect();
+        let mut rng = thread_rng();
+        rng.shuffle(&mut indices);
+
+        let position = reshuffle(
+            &indices,
+            node_data.position,
+            match node_data.meta.position_encoding {
+                octree::PositionEncoding::Uint8 => 3,
+                octree::PositionEncoding::Uint16 => 6,
+                octree::PositionEncoding::Float32 => 12,
+            },
+        );
+        let color = reshuffle(&indices, node_data.color, 3);
+
         let buffer_position = GlBuffer::new();
         let buffer_color = GlBuffer::new();
 
@@ -117,8 +150,8 @@ impl NodeView {
             };
             gl::BufferData(
                 gl::ARRAY_BUFFER,
-                node_data.position.len() as GLsizeiptr,
-                mem::transmute(&node_data.position[0]),
+                position.len() as GLsizeiptr,
+                mem::transmute(&position[0]),
                 gl::STATIC_DRAW,
             );
 
@@ -137,8 +170,8 @@ impl NodeView {
             buffer_color.bind();
             gl::BufferData(
                 gl::ARRAY_BUFFER,
-                node_data.color.len() as GLsizeiptr,
-                mem::transmute(&node_data.color[0]),
+                color.len() as GLsizeiptr,
+                mem::transmute(&color[0]),
                 gl::STATIC_DRAW,
             );
             let color_attr = gl::GetAttribLocation(program.id, c_str!("color"));
@@ -174,7 +207,7 @@ fn main() {
         .get_matches();
 
     let octree_directory = PathBuf::from(matches.value_of("octree_directory").unwrap());
-    let otree = octree::Octree::new(&octree_directory).unwrap();
+    let octree = octree::Octree::new(&octree_directory).unwrap();
 
     let ctx = sdl2::init().unwrap();
     let video_subsystem = ctx.video().unwrap();
@@ -218,11 +251,13 @@ fn main() {
 
     let m = camera.get_world_to_gl();
     let mut node_views = Vec::new();
-    for node in otree.get_visible_nodes(&m, camera.width, camera.height, octree::UseLod::No) {
+    let visible_nodes = octree
+        .get_visible_nodes(&m, camera.width, camera.height, octree::UseLod::No);
+    for node in &visible_nodes {
         // We always request nodes at full resolution (i.e. not subsampled by the backend), because
         // we can just as effectively subsample the number of points we draw in the client.
         const ALL_POINTS_LOD: i32 = 1;
-        let node_data = otree.get_node_data(&node.id, ALL_POINTS_LOD).unwrap();
+        let node_data = octree.get_node_data(&node.id, ALL_POINTS_LOD).unwrap();
         node_views.push(NodeView::new(&node_drawer.program, node_data));
     }
 
@@ -280,8 +315,8 @@ fn main() {
             gl::ClearColor(0., 1., 0., 1.);
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
-            for view in &node_views {
-                num_points_drawn += node_drawer.draw(view);
+            for (i, visible_node) in visible_nodes.iter().enumerate() {
+                num_points_drawn += node_drawer.draw(&node_views[i], visible_node.level_of_detail);
             }
         }
 
