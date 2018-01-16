4e0433129e5fec76c6510aecb97ea7d44e673686
thomasschiwietz <tschiwietz@lyft.com>
2018-01-16 09:59:37 +0100

    Use projection matrix from cgmath

--- sdl_viewer/src/camera.rs
+++ sdl_viewer/src/camera.rs
@@ -12,55 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use cgmath::{Angle, Decomposed, Deg, InnerSpace, Matrix4, One, Quaternion, Rad, Rotation,
+use cgmath::{Decomposed, Deg, InnerSpace, Matrix4, One, Quaternion, PerspectiveFov, Rad, Rotation,
              Rotation3, Transform, Vector3, Zero};
 use opengl;
 use std::f32;
-
-// Constructs a projection matrix. Math lifted from ThreeJS.
-fn make_projection_matrix<A: Into<Rad<f32>>>(
-    near: f32,
-    far: f32,
-    fov: A,
-    zoom: f32,
-    aspect_ratio: f32,
-) -> Matrix4<f32> {
-    let top = 0.5 * near * fov.into().tan() / zoom;
-    let height = 2. * top;
-    let width = aspect_ratio * height;
-    let left = -0.5 * width;
-
-    let right = left + width;
-    let bottom = top - height;
-
-    // Matrix is column major.
-    let x = 2. * near / (right - left);
-    let y = 2. * near / (top - bottom);
-
-    let a = (right + left) / (right - left);
-    let b = (top + bottom) / (top - bottom);
-    let c = -(far + near) / (far - near);
-    let d = -2. * far * near / (far - near);
-
-    Matrix4::new(
-        x,
-        0.,
-        0.,
-        0., // Column 0
-        0.,
-        y,
-        0.,
-        0., // Column 1
-        a,
-        b,
-        c,
-        -1., // Column 2
-        0.,
-        0.,
-        d,
-        0., // Column 3
-    )
-}
 
 #[derive(Debug)]
 pub struct Camera {
@@ -114,8 +69,7 @@ impl Camera {
     pub fn set_size(&mut self, gl: &opengl::Gl, width: i32, height: i32) {
         self.width = width;
         self.height = height;
-        self.projection_matrix =
-            make_projection_matrix(0.1, 10000., Deg(45.), 1., width as f32 / height as f32);
+        self.projection_matrix = Matrix4::from(PerspectiveFov { fovy: Rad::from(Deg(45.)), aspect: width as f32 / height as f32, near: 0.1, far: 10000.0 });
         unsafe {
             gl.Viewport(0, 0, width, height);
         }
