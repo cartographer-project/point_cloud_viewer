332aadc21a1de50eb0b8514b8df635cc79ebd216
Holger Rapp <HolgerRapp@gmx.net>
2018-01-22 13:52:58 +0100

    Use the workspace feature. (#82)
    Please rm -rf */target */Cargo.lock.
    
    This might speed up CI as well, since now the correct directory is
    cached.

--- ci/script.sh
+++ ci/script.sh
@@ -1,32 +1,14 @@
 set -ex
 
 main() {
-    # root
-    cargo build --verbose
-    cargo test
-
-    # grpc
-    pushd grpc
-    cargo build --verbose
-    cargo test
-    popd
-
     # web viewer - client
-    pushd web_viewer
-    pushd client
+    pushd web_viewer/client
     npm run build
     popd
 
-    # web viewer - server
-    cargo build --verbose
-    cargo test
-    popd
-
-    # sdl viewer
-    pushd sdl_viewer
-    cargo build --verbose
-    cargo test
-    popd
+    # root
+    cargo build --all --verbose
+    cargo test --all
 }
 
 main
