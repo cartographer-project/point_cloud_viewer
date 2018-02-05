4477a6849a2068e55f2084e812150f5ffc6c41ff
Patrick Dubroy <pdubroy@lyft.com>
2018-02-05 13:09:57 +0100

    Replace TypeScript build step with Rollup.
    This allows us to bundle third party dependencies into the main bundle.
    Also removed tsft, as it wasn’t much use.
    
    PAIR=SirVer

---
+++ xray/client/rollup.config.js
@@ -0,0 +1,22 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+
+export default {
+    input: 'main.ts',
+    output: {
+        format: 'iife',
+        file: '../../target/xray_app_bundle.js',
+        sourcemap: true
+    },
+    plugins: [
+        commonjs({
+            include: 'node_modules/**',
+        }),
+        nodeResolve({
+            browser: true,
+            preferBuiltins: false,
+        }),
+        typescript()
+    ],
+};
