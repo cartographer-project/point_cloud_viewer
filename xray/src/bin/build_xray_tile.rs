b1c345a28fc1a065f29e503ed1b07d343071e56d
Holger Rapp <HolgerRapp@gmx.net>
2018-05-03 11:48:15 +0200

    Adds a binary that generates a single X-Ray tile. (#131)

---
+++ xray/src/bin/build_xray_tile.rs
@@ -0,0 +1,124 @@
+extern crate cgmath;
+#[macro_use]
+extern crate clap;
+extern crate collision;
+extern crate fnv;
+extern crate image;
+extern crate point_viewer;
+extern crate protobuf;
+extern crate quadtree;
+extern crate scoped_pool;
+extern crate xray;
+
+use cgmath::{Point2, Point3};
+use collision::{Aabb, Aabb2, Aabb3};
+use octree::OnDiskOctree;
+use point_viewer::octree;
+use std::error::Error;
+use std::path::Path;
+use xray::generation::{xray_from_points, ColoringStrategyKind};
+
+fn parse_arguments() -> clap::ArgMatches<'static> {
+    clap::App::new("build_xray_tile")
+        .version("1.0")
+        .author("Holger H. Rapp <hrapp@lyft.com>")
+        .args(&[
+            clap::Arg::with_name("output_filename")
+                .help("Output filename to write into.")
+                .default_value("output.png")
+                .long("output_filename")
+                .takes_value(true),
+            clap::Arg::with_name("resolution")
+                .help("Size of 1px in meters.")
+                .long("resolution")
+                .default_value("0.01"),
+            clap::Arg::with_name("coloring_strategy")
+                .long("coloring_strategy")
+                .takes_value(true)
+                .possible_values(&ColoringStrategyKind::variants())
+                .default_value("xray"),
+            clap::Arg::with_name("octree_directory")
+                .help("Octree directory to turn into xrays.")
+                .index(1)
+                .required(true),
+            clap::Arg::with_name("min_x")
+                .long("min_x")
+                .takes_value(true)
+                .help("Bounding box minimum x in meters.")
+                .required(true),
+            clap::Arg::with_name("min_y")
+                .long("min_y")
+                .takes_value(true)
+                .help("Bounding box minimum y in meters.")
+                .required(true),
+            clap::Arg::with_name("max_x")
+                .long("max_x")
+                .takes_value(true)
+                .help("Bounding box maximum x in meters.")
+                .required(true),
+            clap::Arg::with_name("max_y")
+                .long("max_y")
+                .takes_value(true)
+                .help("Bounding box maximum y in meters.")
+                .required(true),
+        ])
+        .get_matches()
+}
+
+fn run(
+    octree_directory: &Path,
+    output_filename: &Path,
+    resolution: f32,
+    coloring_strategy_kind: ColoringStrategyKind,
+    bbox2: &Aabb2<f32>,
+) -> Result<(), Box<Error>> {
+    let octree = &OnDiskOctree::new(octree_directory)?;
+    let bbox3 = octree.bounding_box();
+    let bbox3 = Aabb3::new(
+        Point3::new(
+            bbox2.min().x.max(bbox3.min().x),
+            bbox2.min().y.max(bbox3.min().y),
+            bbox3.min().z,
+        ),
+        Point3::new(
+            bbox2.max().x.min(bbox3.max().x),
+            bbox2.max().y.min(bbox3.max().y),
+            bbox3.max().z,
+        ),
+    );
+    let image_width = (bbox2.dim().x / resolution).ceil() as u32;
+    let image_height = (bbox2.dim().y / resolution).ceil() as u32;
+    if !xray_from_points(
+        octree,
+        &bbox3,
+        output_filename,
+        image_width,
+        image_height,
+        coloring_strategy_kind.new_strategy(),
+    ) {
+        println!("No points in bounding box. No output written.");
+    }
+    Ok(())
+}
+
+pub fn main() {
+    let matches = parse_arguments();
+    let resolution = value_t!(matches, "resolution", f32).expect("resolution could not be parsed.");
+    let coloring_strategy_kind = value_t!(matches, "coloring_strategy", ColoringStrategyKind)
+        .expect("coloring_strategy is invalid");
+    let octree_directory = Path::new(matches.value_of("octree_directory").unwrap());
+    let output_filename = Path::new(matches.value_of("output_filename").unwrap());
+    let min_x = value_t!(matches, "min_x", f32).expect("min_x could not be parsed.");
+    let min_y = value_t!(matches, "min_y", f32).expect("min_y could not be parsed.");
+    let max_x = value_t!(matches, "max_x", f32).expect("max_x could not be parsed.");
+    let max_y = value_t!(matches, "max_y", f32).expect("max_y could not be parsed.");
+
+    let bbox2 = Aabb2::new(Point2::new(min_x, min_y), Point2::new(max_x, max_y));
+    run(
+        octree_directory,
+        output_filename,
+        resolution,
+        coloring_strategy_kind,
+        &bbox2,
+    ).unwrap();
+}
