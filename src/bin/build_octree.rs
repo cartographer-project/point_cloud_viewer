cea065d4b91601d678d392a80ae7505a00598b64
Avinash Kumar <akumar@lyft.com>
2018-09-04 11:11:59 +0200

    Fix random number range issue

--- src/bin/build_octree.rs
+++ src/bin/build_octree.rs
@@ -48,7 +48,7 @@ use std::{mem, thread};
 use rand::Rng;
 use std::collections::HashMap;
 
-const MAX_NODES_IN_CACHE: usize = 1000;
+const MAX_NODES_IN_CACHE: usize = 10000;
 const UPDATE_COUNT: i64 = 100000;
 const MAX_POINTS_PER_NODE: i64 = 10000;
 
@@ -88,19 +88,22 @@ impl<'a> OctreeBuilder<'a> {
         node_id:  &octree::NodeId,
         points: &Vec<point_viewer::Point>
     ) {
-        match self.cache.remove(&node_id) {
-            Some(points) => {
-                println!("Removed points in cache for node id {}", node_id);
-                match self.cache.insert(*node_id, points) {
-                    Some(points) => println!("Insert node id {} in cache", node_id),
-                    None => panic!("Cannot insert node id {} in cache", node_id),
-                }
-            },
-            None => {
-                println!("Didn't read points from cache for node id {}", node_id);
-                self.cache.insert(*node_id, points.to_vec());
-            },
-        }
+        self.cache.remove(&node_id);
+        self.cache.insert(*node_id, points.to_vec());
+        //
+        // match self.cache.remove(&node_id) {
+        //     Some(points) => {
+        //         println!("Removed points in cache for node id {}", node_id);
+        //         match self.cache.insert(*node_id, points) {
+        //             Some(points) => println!("Insert node id {} in cache", node_id),
+        //             None => panic!("Cannot insert node id {} in cache", node_id),
+        //         }
+        //     },
+        //     None => {
+        //         println!("Didn't read points from cache for node id {}", node_id);
+        //         self.cache.insert(*node_id, points.to_vec());
+        //     },
+        // }
     }
 
     // Read from cache or disk
@@ -110,12 +113,11 @@ impl<'a> OctreeBuilder<'a> {
     ) -> Vec<point_viewer::Point> {
         let points = match self.cache.get_mut(&node_id) {
             Some(cached_points) => {
-                println!("Found points in cache for {}", node_id);
                 cached_points.clone()
             },
             // Case: The current node is empty
             None => {
-                println!("Reading points from cache {}", node_id);
+                //println!("Reading points from cache {}", node_id);
                 let node_iterator = octree::NodeIterator::from_disk(&self.octree_meta, &node_id).unwrap();
                 let mut pd = &mut Vec::with_capacity(node_iterator.size_hint().unwrap());
                 node_iterator.for_each(|p| pd.push((*p).clone()));
@@ -136,21 +138,22 @@ impl<'a> OctreeBuilder<'a> {
         let child_index = octree::ChildIndex::from_bounding_cube(&bounding_cube, &point.position);
         let child_id = node_id.get_child_id(child_index);
         let child_bounding_cube = child_id.find_bounding_cube(&bounding_cube);
-        *self.points_seen_in_node.entry(*node_id).or_insert(0) += 1;
 
         // Case 1: New node with 0 points
         if !self.points_seen_in_node.contains_key(&node_id) {
+            *self.points_seen_in_node.entry(*node_id).or_insert(0) += 1;
             let mut node_writer = octree::NodeWriter::new(&self.octree_meta, &node_id);
             node_writer.write(&point);
             return;
         }
 
+        *self.points_seen_in_node.entry(*node_id).or_insert(0) += 1;
         let mut points = self.read_cache_or_disk(&node_id);
         let mut node_writer = octree::NodeWriter::new(&self.octree_meta, &node_id);
         let points_seen = self.points_seen_in_node[&node_id];
 
         // Case 1: The current node has less than or equal to MAX_POINTS_PER_NODE points
-        if points_seen < MAX_POINTS_PER_NODE as i64 {
+        if points_seen <= MAX_POINTS_PER_NODE as i64 {
             points.push(point.clone());
             self.insert_or_update_cache(&node_id, &points);
             for p in points {
