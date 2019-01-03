dd0b967484805b1e3394774cdc9faad4fdf7f556
Sasha Kovalyov <alexander.kovaleff@gmail.com>
2019-01-03 15:03:11 +0100

    introduce AsyncTasksQueue that might be helpful when scheduling the execution of async operations

---
+++ octree_web_viewer/client/lib/async-tasks-queue.ts
@@ -0,0 +1,42 @@
+type Task<T> = {
+  fn: Function,
+  resolve: Function,
+  reject: Function
+};
+
+export default class AsyncTasksQueue<T> {
+
+  private queue: Task<T>[];
+  private pendingCount: number;
+
+  constructor(
+    public maxConcurrentTasks: number
+  ) {
+    this.queue = [];
+    this.pendingCount = 0;
+  }
+
+  public addTask(fn: () => Promise<T>): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      this.queue.push({ fn, resolve, reject });
+      this.processQueue();
+    });
+  }
+
+  private processQueue() {
+    this.queue.splice(0, this.maxConcurrentTasks - this.pendingCount)
+      .forEach(
+        ({ fn, resolve, reject }) => {
+          this.pendingCount++;
+          fn()
+            .then(resolve, reject)
+            .finally(() => {
+              this.pendingCount--;
+              this.processQueue();
+            });
+        }
+      );
+  }
+}
+
+
