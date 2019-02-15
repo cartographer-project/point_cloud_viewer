56feaf383dcd6872e7a58ee9cdce8fd5b182b44b
Marco Feuerstein <mfeuerstein@lyft.com>
2019-02-15 12:48:06 +0100

    ...

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -3,7 +3,7 @@
 set -ex
 
 main() {
-    [[ "$TRAVIS_RUST_VERSION" == "nightly" ]] && nightly_suffix+=("--toolchain=nightly || cargo install --git https://github.com/rust-lang/rust-clippy/ --force clippy")
+    [[ "$TRAVIS_RUST_VERSION" == "nightly" ]] && nightly_suffix+=(--toolchain=nightly\ \|\|\ cargo\ install\ --git\ https://github.com/rust-lang/rust-clippy/\ --force\ clippy)
     rustup component add clippy "${nightly_suffix[@]}"
 }
 
