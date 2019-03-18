f73b076fc2dcc3e5ce1c44f14644577e671d9979
Marco Feuerstein <mfeuerstein@lyft.com>
2019-03-18 14:22:57 +0100

    Renaming

--- sdl_viewer/src/node_drawer.rs
+++ sdl_viewer/src/node_drawer.rs
@@ -54,8 +54,8 @@ pub struct NodeProgram {
 }
 
 pub struct NodeDrawer {
-    program_float: NodeProgram,
-    program_double: NodeProgram,
+    program_f32: NodeProgram,
+    program_f64: NodeProgram,
 }
 
 impl NodeDrawer {
@@ -85,23 +85,23 @@ impl NodeDrawer {
                 u_min,
             }
         };
-        let program_float = create_program(VERTEX_SHADER);
-        let program_double = create_program(
+        let program_f32 = create_program(VERTEX_SHADER);
+        let program_f64 = create_program(
             &VERTEX_SHADER
                 .to_string()
                 .replace("#define POS_TYPE vec3", "#define POS_TYPE dvec3"),
         );
         NodeDrawer {
-            program_float,
-            program_double,
+            program_f32,
+            program_f64,
         }
     }
 
     pub fn program(&self, meta: &octree::NodeMeta) -> &NodeProgram {
         if meta.position_encoding == octree::PositionEncoding::Float64 {
-            &self.program_double
+            &self.program_f64
         } else {
-            &self.program_float
+            &self.program_f32
         }
     }
 
@@ -115,8 +115,8 @@ impl NodeDrawer {
                 matrix.cast::<f64>().unwrap().as_ptr(),
             );
         };
-        update_matrix(&mut self.program_float);
-        update_matrix(&mut self.program_double);
+        update_matrix(&mut self.program_f32);
+        update_matrix(&mut self.program_f64);
     }
 
     pub fn draw(
