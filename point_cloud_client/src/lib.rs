dbf53505dcf1b7267f2dede86bb2ae393454139b
Marco Feuerstein <mfeuerstein@lyft.com>
2019-05-20 14:10:28 +0200

    Add point cloud client.

---
+++ point_cloud_client/src/lib.rs
@@ -0,0 +1,39 @@
+use point_viewer::errors::*;
+use point_viewer::octree::{
+    BatchIterator, Octree, OctreeFactory, PointLocation, NUM_POINTS_PER_BATCH,
+};
+use point_viewer::PointData;
+
+pub struct PointCloudClient {
+    octrees: Vec<Octree>,
+    pub num_points_per_batch: usize,
+}
+
+impl PointCloudClient {
+    pub fn new(locations: &[String], octree_factory: OctreeFactory) -> Result<Self> {
+        let octrees: Result<Vec<Octree>> = locations
+            .iter()
+            .map(|location| {
+                octree_factory
+                    .generate_octree(location)
+                    .map(|octree| *octree)
+            })
+            .collect();
+        octrees.map(|octrees| PointCloudClient {
+            octrees,
+            num_points_per_batch: NUM_POINTS_PER_BATCH,
+        })
+    }
+
+    pub fn for_each_point_data<F>(&self, point_location: &PointLocation, mut func: F) -> Result<()>
+    where
+        F: FnMut(PointData) -> Result<()>,
+    {
+        for octree in &self.octrees {
+            let mut batch_iterator =
+                BatchIterator::new(octree, point_location, self.num_points_per_batch);
+            batch_iterator.try_for_each_batch(&mut func)?;
+        }
+        Ok(())
+    }
+}
