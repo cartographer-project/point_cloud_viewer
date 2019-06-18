1f39277499526e71a330d9625780923f81413386
Marco Feuerstein <mfeuerstein@lyft.com>
2019-06-18 10:49:40 +0200

    Now using ENU to calculate center offsets.

--- src/s2_geo.rs
+++ src/s2_geo.rs
@@ -1,5 +1,4 @@
-use cgmath::Vector2;
-use nav_types::{ECEF, WGS84};
+use nav_types::{ECEF, ENU, WGS84};
 use s2::cap::Cap;
 use s2::cellid::CellID;
 use s2::latlng::LatLng;
@@ -7,66 +6,53 @@ use s2::point::Point;
 use s2::rect::Rect;
 use s2::region::RegionCoverer;
 use s2::s1::angle::{Angle, Rad};
-use s2::s1::chordangle::ChordAngle;
+use std::ops::Add;
 use std::vec::Vec;
 
 const LEVEL_MOD: u8 = 1;
 
-// These consts are publicly defined in https://github.com/nordmoen/nav-types/blob/master/src/wgs84.rs,
-// but unfortunately not exposed via the lib.
-const INVERSE_FLATTENING: f64 = 298.257_223_563;
-const FLATTENING: f64 = 1.0 / INVERSE_FLATTENING;
-const SEMI_MAJOR_AXIS: f64 = 6_378_137.0;
-
-// See https://www.eoas.ubc.ca/~mjelline/Planetary%20class/14gravity1_2.pdf for the formulas.
-fn radius_at(lat_rad: f64) -> f64 {
-    let lat_rad_sin = lat_rad.sin();
-    SEMI_MAJOR_AXIS * (1.0 - FLATTENING * lat_rad_sin * lat_rad_sin)
-}
-
-// TODO(feuerste): This approximates the angle for a point lying on the surface of the earth.
-// The tangent is assumed to be tangential to a sphere, not an ellipsoid.
-fn tangent_m_to_rad_approx(meters: f64, lat_rad: f64) -> f64 {
-    2.0 * (0.5 * meters / radius_at(lat_rad)).atan()
-}
-
 trait LatLngExt {
     fn from(_: Self) -> LatLng;
 }
-impl LatLngExt for (f64, f64) {
-    fn from(lat_lng_rad: Self) -> LatLng {
-        let lat = Angle::from(Rad(lat_lng_rad.0));
-        let lng = Angle::from(Rad(lat_lng_rad.1));
+
+impl LatLngExt for WGS84<f64> {
+    fn from(wgs: Self) -> LatLng {
+        let lat = Angle::from(Rad(wgs.latitude()));
+        let lng = Angle::from(Rad(wgs.longitude()));
         LatLng::new(lat, lng)
     }
 }
-impl LatLngExt for WGS84<f64> {
-    fn from(wgs: Self) -> LatLng {
-        LatLngExt::from((wgs.latitude(), wgs.longitude()))
-    }
-}
+
 impl LatLngExt for ECEF<f64> {
     fn from(ecef: Self) -> LatLng {
         LatLngExt::from(std::convert::Into::<WGS84<f64>>::into(ecef))
     }
 }
 
-pub fn cell_id(ecef: ECEF<f64>, level: u8) -> CellID {
-    CellID::from(LatLngExt::from(ecef)).parent(u64::from(level))
+pub fn cell_id(ecef_m: ECEF<f64>, level: u8) -> CellID {
+    CellID::from(LatLngExt::from(ecef_m)).parent(u64::from(level))
 }
 
-pub fn cell_ids_rect_approx(
+pub fn cell_ids_rect(
     ecef_m: ECEF<f64>,
-    extent_m: Vector2<f64>,
+    half_extent_m: ENU<f64>,
     level: u8,
     max_num_cells: usize,
 ) -> Vec<CellID> {
     let center = LatLngExt::from(ecef_m);
-    let size = LatLngExt::from((
-        tangent_m_to_rad_approx(extent_m.x, center.lat.rad()),
-        tangent_m_to_rad_approx(extent_m.y, center.lat.rad()),
-    ));
-    let rect = Rect::from_center_size(center, size);
+    let rect = vec![(-1., -1.), (-1., 1.), (1., -1.), (1., 1.)]
+        .into_iter()
+        .map(|fac| {
+            Rect::from(LatLngExt::from(
+                ecef_m
+                    + ENU::new(
+                        fac.0 * half_extent_m.east(),
+                        fac.1 * half_extent_m.north(),
+                        -half_extent_m.up(),
+                    ),
+            ))
+        })
+        .fold(Rect::from(center), |rect, corner| rect.union(&corner));
     let cov = RegionCoverer {
         min_level: level,
         max_level: level,
@@ -84,12 +70,17 @@ pub fn cell_ids_radius_approx(
     max_num_cells: usize,
 ) -> Vec<CellID> {
     let center = LatLngExt::from(ecef_m);
-    let radius_rad = tangent_m_to_rad_approx(radius_m, center.lat.rad());
-    let length2 = radius_rad * radius_rad;
-    let cap = Cap::from_center_chordangle(
-        &Point::from(center),
-        &ChordAngle::from_squared_length(length2),
-    );
+    let cap = vec![(0., -1.), (-1., 0.), (1., 0.), (0., 1.)]
+        .into_iter()
+        .map(|fac| {
+            Point::from(LatLngExt::from(
+                // To be on the safe side, we translate the up vector by -radius, too.
+                ecef_m + ENU::new(fac.0 * radius_m, fac.1 * radius_m, -radius_m),
+            ))
+        })
+        .fold(Cap::from(&Point::from(center)), |cap, corner| {
+            cap.add(&corner)
+        });
     let cov = RegionCoverer {
         min_level: level,
         max_level: level,
@@ -106,15 +97,6 @@ mod tests {
     use s2::cellid::MAX_LEVEL;
 
     #[test]
-    fn test_radius() {
-        assert_eq!(SEMI_MAJOR_AXIS, radius_at(0.0));
-        assert_eq!(
-            SEMI_MAJOR_AXIS * (1.0 - FLATTENING),
-            radius_at(std::f64::consts::FRAC_PI_2)
-        );
-    }
-
-    #[test]
     fn test_cell_id() {
         // Hofbräuhaus Munich.
         let ecef = ECEF::new(4_177_536., 855_997., 4_727_102.);
@@ -127,19 +109,19 @@ mod tests {
     #[test]
     fn test_region_coverer() {
         // Defines a 5x5 m region in Palo Alto.
-        let length = 5.0;
+        let radius = 2.5;
         let ecef_pao = ECEF::new(-2699112.642037565, -4294985.7005343605, 3853368.5643771216);
-        let extent = Vector2::new(length, length);
+        let half_extent = ENU::new(radius, radius, radius);
 
         let tokens_ground_truth = vec!["808fba97497", "808fba97499"];
 
-        let tokens_rect: Vec<String> = cell_ids_rect_approx(ecef_pao, extent, 20, 1000)
+        let tokens_rect: Vec<String> = cell_ids_rect(ecef_pao, half_extent, 20, 1000)
             .into_iter()
             .map(|cell_id| cell_id.to_token())
             .collect();
         assert_eq!(tokens_ground_truth, tokens_rect);
 
-        let tokens_radius: Vec<String> = cell_ids_radius_approx(ecef_pao, 0.5 * length, 20, 1000)
+        let tokens_radius: Vec<String> = cell_ids_radius_approx(ecef_pao, radius, 20, 1000)
             .into_iter()
             .map(|cell_id| cell_id.to_token())
             .collect();
