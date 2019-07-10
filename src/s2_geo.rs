eec823d1fae29fb9aa92fd67e7b8cc6637e58259
Marco Feuerstein <mfeuerstein@lyft.com>
2019-07-10 13:08:10 +0200

    Beautified from syntax.

--- src/s2_geo.rs
+++ src/s2_geo.rs
@@ -10,30 +10,30 @@ use std::vec::Vec;
 const LEVEL_MOD: u8 = 1;
 
 pub trait ECEFExt<N> {
-    fn from(_: Self) -> ECEF<N>;
+    fn from(self) -> ECEF<N>;
 }
 
 impl<N> ECEFExt<N> for Vector3<N> {
-    fn from(vec: Self) -> ECEF<N> {
-        ECEF::new(vec.x, vec.y, vec.z)
+    fn from(self) -> ECEF<N> {
+        ECEF::new(self.x, self.y, self.z)
     }
 }
 
 trait LatLngExt {
-    fn from(_: Self) -> LatLng;
+    fn from(self) -> LatLng;
 }
 
 impl LatLngExt for WGS84<f64> {
-    fn from(wgs: Self) -> LatLng {
-        let lat = Angle::from(Rad(wgs.latitude()));
-        let lng = Angle::from(Rad(wgs.longitude()));
+    fn from(self) -> LatLng {
+        let lat = Angle::from(Rad(self.latitude()));
+        let lng = Angle::from(Rad(self.longitude()));
         LatLng::new(lat, lng)
     }
 }
 
 impl LatLngExt for ECEF<f64> {
-    fn from(ecef: Self) -> LatLng {
-        LatLngExt::from(std::convert::Into::<WGS84<f64>>::into(ecef))
+    fn from(self) -> LatLng {
+        LatLngExt::from(std::convert::Into::<WGS84<f64>>::into(self))
     }
 }
 
