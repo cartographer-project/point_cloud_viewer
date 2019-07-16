b297d962eddb49a2df8c38fca3fd82b9a29d298e
Nikolai Morin <nmorin@lyft.com>
2019-07-16 16:46:23 +0200

    Use std::slice::from_ref to prettify code

--- point_viewer_grpc/src/service.rs
+++ point_viewer_grpc/src/service.rs
@@ -37,7 +37,7 @@ use std::str::FromStr;
 use std::sync::{Arc, RwLock};
 
 struct OctreeServiceData {
-    octree: [Octree; 1], // caveat: only one octree in this slice
+    octree: Box<Octree>,
     meta: point_viewer::proto::Meta,
 }
 
@@ -95,7 +95,7 @@ impl proto_grpc::Octree for OctreeService {
             Ok(node_id) => node_id,
             Err(e) => return send_fail(&ctx, sink, e.to_string()),
         };
-        let node_data = match service_data.octree[0].get_node_data(&node_id) {
+        let node_data = match service_data.octree.get_node_data(&node_id) {
             Ok(data) => data,
             Err(e) => return send_fail(&ctx, sink, e.to_string()),
         };
@@ -332,7 +332,7 @@ impl OctreeService {
                 };
 
                 let mut batch_iterator = BatchIterator::new(
-                    &service_data.octree,
+                    std::slice::from_ref(&service_data.octree),
                     &query,
                     num_points_per_batch,
                     num_cpus::get() - 1,
@@ -356,14 +356,11 @@ impl OctreeService {
         if let Some(service_data) = self.data_cache.read().unwrap().get(octree_id) {
             return Ok(Arc::clone(service_data));
         };
-        let octree_slice: [Octree; 1] = [*self
+        let octree = self
             .factory
-            .generate_octree(self.location.join(&octree_id).to_string_lossy())?];
-        let meta = octree_slice[0].to_meta_proto();
-        let service_data = Arc::new(OctreeServiceData {
-            octree: octree_slice,
-            meta,
-        });
+            .generate_octree(self.location.join(&octree_id).to_string_lossy())?;
+        let meta = octree.to_meta_proto();
+        let service_data = Arc::new(OctreeServiceData { octree, meta });
         self.data_cache
             .write()
             .unwrap()
