cartographer-project/point_cloud_viewer

commit 1d337b311d555cfe701e567ebc0f835b7aa0da37

Make x-ray background transparent (#198)
### Desc
As a UI developer, I would like the background color of x-rays tiles to be transparent, so that I can easily overlay them on other map data.

xray/src/generation.rs
@@ -9,7 +9,7 @@ use fnv::{FnvHashMap, FnvHashSet};
 use image::{self, GenericImage};
 use point_viewer::math::clamp;
 use point_viewer::{
-    color::{Color, WHITE},
+    color::Color,
     octree, InternalIterator, Point,
 };
 use protobuf::Message;
@@ -87,6 +87,15 @@ arg_enum! {
     }
 }
 
+arg_enum! {
+    #[derive(Debug)]
+    #[allow(non_camel_case_types)]
+    pub enum TileBackgroundColorArgument {
+        white,
+        transparent,
+    }
+}
+
 #[derive(Debug)]
 pub enum ColoringStrategyKind {
     XRay,
@@ -120,7 +129,7 @@ pub trait ColoringStrategy: Send {
 
     // After all points are processed, this is used to query the color that should be assigned to
     // the pixel (x, y) in the final tile image.
-    fn get_pixel_color(&self, x: u32, y: u32) -> Color<u8>;
+    fn get_pixel_color(&self, x: u32, y: u32, background_color: Color<u8>) -> Color<u8>;
 }
 
 struct XRayColoringStrategy {
@@ -152,9 +161,9 @@ impl ColoringStrategy for XRayColoringStrategy {
         }
     }
 
-    fn get_pixel_color(&self, x: u32, y: u32) -> Color<u8> {
+    fn get_pixel_color(&self, x: u32, y: u32, background_color: Color<u8>) -> Color<u8> {
         if !self.z_buckets.contains_key(&(x, y)) {
-            return WHITE.to_u8();
+            return background_color;
         }
         let saturation = (self.z_buckets[&(x, y)].len() as f32).ln() / self.max_saturation;
         let value = ((1. - saturation) * 255.) as u8;
@@ -211,9 +220,9 @@ impl ColoringStrategy for IntensityColoringStrategy {
         }
     }
 
-    fn get_pixel_color(&self, x: u32, y: u32) -> Color<u8> {
+    fn get_pixel_color(&self, x: u32, y: u32, background_color: Color<u8>) -> Color<u8> {
         if !self.per_column_data.contains_key(&(x, y)) {
-            return WHITE.to_u8();
+            return background_color;
         }
         let c = &self.per_column_data[&(x, y)];
         let mean = (c.sum / c.count as f32).max(self.min).min(self.max);
@@ -262,9 +271,9 @@ impl ColoringStrategy for PointColorColoringStrategy {
         }
     }
 
-    fn get_pixel_color(&self, x: u32, y: u32) -> Color<u8> {
+    fn get_pixel_color(&self, x: u32, y: u32, background_color: Color<u8>) -> Color<u8> {
         if !self.per_column_data.contains_key(&(x, y)) {
-            return WHITE.to_u8();
+            return background_color;
         }
         let c = &self.per_column_data[&(x, y)];
         Color {
@@ -294,7 +303,7 @@ impl HeightStddevColoringStrategy {
 /// Build a parent image created of the 4 children tiles. All tiles are optionally, in which case
 /// they are left white in the resulting image. The input images must be square with length N,
 /// the returned image is square with length 2*N.
-pub fn build_parent(children: &[Option<image::RgbImage>]) -> image::RgbImage {
+pub fn build_parent(children: &[Option<image::RgbaImage>], tile_background_color: Color<u8>) -> image::RgbaImage {
     assert_eq!(children.len(), 4);
     let mut child_size_px = None;
     for c in children.iter() {
@@ -315,11 +324,16 @@ pub fn build_parent(children: &[Option<image::RgbImage>]) -> image::RgbImage {
         }
     }
     let child_size_px = child_size_px.expect("No children passed to 'build_parent'.");
-    let mut large_image = image::RgbImage::from_pixel(
+    let mut large_image = image::RgbaImage::from_pixel(
         child_size_px * 2,
         child_size_px * 2,
-        image::Rgb {
-            data: [255, 255, 255],
+        image::Rgba {
+            data: [
+                tile_background_color.red,
+                tile_background_color.green,
+                tile_background_color.blue,
+                tile_background_color.alpha,
+            ],
         },
     );
 
@@ -347,9 +361,9 @@ impl ColoringStrategy for HeightStddevColoringStrategy {
             .add(p.position.z);
     }
 
-    fn get_pixel_color(&self, x: u32, y: u32) -> Color<u8> {
+    fn get_pixel_color(&self, x: u32, y: u32, background_color: Color<u8>) -> Color<u8> {
         if !self.per_column_data.contains_key(&(x, y)) {
-            return WHITE.to_u8();
+            return background_color;
         }
         let c = &self.per_column_data[&(x, y)];
         let saturation = clamp(c.stddev() as f32, 0., self.max_stddev) / self.max_stddev;
@@ -364,6 +378,7 @@ pub fn xray_from_points(
     image_width: u32,
     image_height: u32,
     mut coloring_strategy: Box<ColoringStrategy>,
+    tile_background_color: Color<u8>,
 ) -> bool {
     let mut seen_any_points = false;
     octree.points_in_box(bbox).for_each(|p| {
@@ -383,15 +398,15 @@ pub fn xray_from_points(
         return false;
     }
 
-    let mut image = image::RgbImage::new(image_width, image_height);
+    let mut image = image::RgbaImage::new(image_width, image_height);
     for x in 0..image_width {
         for y in 0..image_height {
-            let color = coloring_strategy.get_pixel_color(x, y);
+            let color = coloring_strategy.get_pixel_color(x, y, tile_background_color);
             image.put_pixel(
                 x,
                 y,
-                image::Rgb {
-                    data: [color.red, color.green, color.blue],
+                image::Rgba {
+                    data: [color.red, color.green, color.blue, color.alpha],
                 },
             );
         }
@@ -426,6 +441,7 @@ pub fn build_xray_quadtree(
     resolution: f32,
     tile_size_px: u32,
     coloring_strategy_kind: &ColoringStrategyKind,
+    tile_background_color: Color<u8>
 ) -> Result<(), Box<Error>> {
     // Ignore errors, maybe directory is already there.
     let _ = fs::create_dir(output_directory);
@@ -467,6 +483,7 @@ pub fn build_xray_quadtree(
                         tile_size_px,
                         tile_size_px,
                         strategy,
+                        tile_background_color
                     ) {
                         all_nodes_tx_clone.send(node.id).unwrap();
                         if let Some(id) = node.id.parent_id() {
@@ -507,16 +524,16 @@ pub fn build_xray_quadtree(
                         if !png.exists() {
                             continue;
                         }
-                        children[id as usize] = Some(image::open(&png).unwrap().to_rgb());
+                        children[id as usize] = Some(image::open(&png).unwrap().to_rgba());
                     }
-                    let large_image = build_parent(&children);
-                    let image = image::DynamicImage::ImageRgb8(large_image).resize(
+                    let large_image = build_parent(&children, tile_background_color);
+                    let image = image::DynamicImage::ImageRgba8(large_image).resize(
                         tile_size_px,
                         tile_size_px,
                         image::FilterType::Lanczos3,
                     );
                     image
-                        .as_rgb8()
+                        .as_rgba8()
                         .unwrap()
                         .save(&get_image_path(output_directory, node_id))
                         .unwrap();

src/color.rs
@@ -85,3 +85,9 @@ pub const WHITE: Color<f32> = Color {
     blue: 1.,
     alpha: 1.,
 };
+pub const TRANSPARENT: Color<f32> = Color {
+    red: 1.,
+    green: 1.,
+    blue: 1.,
+    alpha: 0.,
+};

xray/src/bin/build_xray_quadtree.rs
@@ -1,8 +1,11 @@
 use clap::value_t;
+use point_viewer::color::{TRANSPARENT, WHITE};
 use point_viewer::octree::octree_from_directory;
 use scoped_pool::Pool;
 use std::path::Path;
-use xray::generation::{ColoringStrategyArgument, ColoringStrategyKind};
+use xray::generation::{
+    ColoringStrategyArgument, ColoringStrategyKind, TileBackgroundColorArgument,
+};
 
 fn parse_arguments() -> clap::ArgMatches<'static> {
     clap::App::new("build_xray_quadtree")
@@ -56,6 +59,11 @@ fn parse_arguments() -> clap::ArgMatches<'static> {
                 .help("Octree directory to turn into xrays.")
                 .index(1)
                 .required(true),
+            clap::Arg::with_name("tile_background_color")
+                .long("tile_background_color")
+                .takes_value(true)
+                .possible_values(&TileBackgroundColorArgument::variants())
+                .default_value("white"),
         ])
         .get_matches()
 }
@@ -93,6 +101,15 @@ pub fn main() {
         }
     };
 
+    let tile_background_color = {
+        let arg = value_t!(args, "tile_background_color", TileBackgroundColorArgument)
+            .expect("tile_background_color is invalid");
+        match arg {
+            white => WHITE.to_u8(),
+            transparent => TRANSPARENT.to_u8(),
+        }
+    };
+
     let octree_directory = Path::new(args.value_of("octree_directory").unwrap());
     let output_directory = Path::new(args.value_of("output_directory").unwrap());
 
@@ -105,6 +122,7 @@ pub fn main() {
         resolution,
         tile_size,
         &coloring_strategy_kind,
+        tile_background_color,
     )
     .unwrap();
 }

xray/src/bin/build_xray_tile.rs
@@ -1,10 +1,13 @@
 use cgmath::{Point2, Point3};
 use clap::value_t;
 use collision::{Aabb, Aabb2, Aabb3};
+use point_viewer::color::{Color, TRANSPARENT, WHITE};
 use point_viewer::octree::octree_from_directory;
 use std::error::Error;
 use std::path::Path;
-use xray::generation::{xray_from_points, ColoringStrategyArgument, ColoringStrategyKind};
+use xray::generation::{
+    xray_from_points, ColoringStrategyArgument, ColoringStrategyKind, TileBackgroundColorArgument,
+};
 
 fn parse_arguments() -> clap::ArgMatches<'static> {
     // TODO(sirver): pull out a function for common args.
@@ -75,6 +78,11 @@ fn parse_arguments() -> clap::ArgMatches<'static> {
                 .takes_value(true)
                 .help("Bounding box maximum y in meters.")
                 .required(true),
+            clap::Arg::with_name("tile_background_color")
+                .long("tile_background_color")
+                .takes_value(true)
+                .possible_values(&TileBackgroundColorArgument::variants())
+                .default_value("white"),
         ])
         .get_matches()
 }
@@ -84,6 +92,7 @@ fn run(
     output_filename: &Path,
     resolution: f32,
     coloring_strategy_kind: &ColoringStrategyKind,
+    tile_background_color: Color<u8>,
     bbox2: &Aabb2<f32>,
 ) -> Result<(), Box<Error>> {
     let octree = &octree_from_directory(octree_directory)?;
@@ -109,6 +118,7 @@ fn run(
         image_width,
         image_height,
         coloring_strategy_kind.new_strategy(),
+        tile_background_color,
     ) {
         println!("No points in bounding box. No output written.");
     }
@@ -134,6 +144,18 @@ pub fn main() {
             ),
         }
     };
+    let tile_background_color = {
+        let arg = value_t!(
+            matches,
+            "tile_background_color",
+            TileBackgroundColorArgument
+        )
+        .expect("tile_background_color is invalid");
+        match arg {
+            TileBackgroundColorArgument::white => WHITE.to_u8(),
+            TileBackgroundColorArgument::transparent => TRANSPARENT.to_u8(),
+        }
+    };
     let octree_directory = Path::new(matches.value_of("octree_directory").unwrap());
     let output_filename = Path::new(matches.value_of("output_filename").unwrap());
     let min_x = value_t!(matches, "min_x", f32).expect("min_x could not be parsed.");
@@ -147,6 +169,7 @@ pub fn main() {
         output_filename,
         resolution,
         &coloring_strategy_kind,
+        tile_background_color,
         &bbox2,
     )
     .unwrap();

commit fbc3d444541256f6ce7d97443f2db244806c8b87

Fix X-Ray wrongly being partially transparent. (#320)
This bug was introduced in #198. When data is available for an X-Ray pixel, it should not be partially transparent, but instead fully opaque.

xray/src/generation.rs
@@ -201,7 +201,7 @@ impl ColoringStrategy for XRayColoringStrategy {
             red: value,
             green: value,
             blue: value,
-            alpha: value,
+            alpha: 255,
         }
     }
 }
@@ -344,7 +344,7 @@ impl ColoringStrategy for PointColorColoringStrategy {
             red: c.color_sum.red / c.count as f32,
             green: c.color_sum.green / c.count as f32,
             blue: c.color_sum.blue / c.count as f32,
-            alpha: c.color_sum.alpha / c.count as f32,
+            alpha: 1.,
         }
         .to_u8()
     }