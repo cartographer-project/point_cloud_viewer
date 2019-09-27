ec602045f9c1fd2d052a44447d44de870ce5b687
Nikolai Morin <nmorin@lyft.com>
2019-09-27 16:47:48 +0200

    PR comments

--- sdl_viewer/src/terrain_drawer/read_write.rs
+++ sdl_viewer/src/terrain_drawer/read_write.rs
@@ -4,7 +4,7 @@ use image::{LumaA, Rgba};
 use point_viewer::math::Isometry3;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::io::{Error, ErrorKind};
+use std::io::{self, ErrorKind};
 
 #[derive(Serialize, Deserialize)]
 pub struct Metadata {
@@ -20,37 +20,35 @@ pub type TextureLoaders = (TiledTextureLoader<LumaA<f32>>, TiledTextureLoader<Rg
 #[allow(dead_code)]
 impl Metadata {
     // Custom serialization of some values
-    pub fn from_dir<P: AsRef<std::path::Path>>(dir: P) -> Result<Self, Error> {
+    pub fn from_dir<P: AsRef<std::path::Path>>(dir: P) -> io::Result<Self> {
         let meta_path = dir.as_ref().join("meta.json");
         let reader = File::open(meta_path)?;
         let meta = serde_json::from_reader(reader).map_err(|e| {
             let msg = format!("Could not parse meta.json: {}", e.to_string());
-            Error::new(ErrorKind::InvalidData, msg)
+            io::Error::new(ErrorKind::InvalidData, msg)
         })?;
         Ok(meta)
     }
 
-    pub fn read_tiles<P: AsRef<std::path::Path>>(&self, dir: P) -> Result<TextureLoaders, Error> {
-        let height_tiles = self.tile_positions.iter().map(|xy| {
-            let (x, y) = xy;
+    pub fn read_tiles<P: AsRef<std::path::Path>>(&self, dir: P) -> io::Result<TextureLoaders> {
+        let height_tiles = self.tile_positions.iter().map(|&(x, y)| {
             let height_file_path = dir.as_ref().join(format!("x{:08}_y{:08}.height", x, y));
-            (*xy, height_file_path)
+            ((x, y), height_file_path)
         });
-        let color_tiles = self.tile_positions.iter().map(|xy| {
-            let (x, y) = xy;
+        let color_tiles = self.tile_positions.iter().map(|&(x, y)| {
             let color_file_path = dir.as_ref().join(format!("x{:08}_y{:08}.color", x, y));
-            (*xy, color_file_path)
+            ((x, y), color_file_path)
         });
         let height_tiles = TiledTextureLoader::new(self.tile_size, height_tiles)?;
         let color_tiles = TiledTextureLoader::new(self.tile_size, color_tiles)?;
         Ok((height_tiles, color_tiles))
     }
 
-    pub fn write<P: AsRef<std::path::Path>>(&self, dir: P) -> Result<(), Error> {
+    pub fn write<P: AsRef<std::path::Path>>(&self, dir: P) -> io::Result<()> {
         let writer = File::create(dir.as_ref().join("meta.json"))?;
         serde_json::to_writer(writer, &self).map_err(|e| {
             let msg = format!("Could not write meta.json: {}", e.to_string());
-            Error::new(ErrorKind::InvalidData, msg)
+            io::Error::new(ErrorKind::InvalidData, msg)
         })
     }
 }
