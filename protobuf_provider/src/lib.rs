4c8493c6feae5cea39193fb86ad2b30c7fe4ae83
Marco Feuerstein <mfeuerstein@lyft.com>
2019-10-17 11:47:51 +0200

    Add compile proto method.

--- protobuf_provider/src/lib.rs
+++ protobuf_provider/src/lib.rs
@@ -12,6 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use itertools::Itertools;
+use std::env;
+use std::ffi::OsStr;
+use std::path::Path;
+use std::process::Command;
+use std::str;
+
 pub const PROTOBUF_BIN_PATH: &str = concat!(env!("OUT_DIR"), "/bin");
 pub const PROTOBUF_LIB_PATH: &str = concat!(env!("OUT_DIR"), "/lib");
 pub const PROTOBUF_INCLUDE_PATH: &str = concat!(env!("OUT_DIR"), "/include");
@@ -22,14 +29,55 @@ pub struct ScopedProtocPath {
 
 impl Default for ScopedProtocPath {
     fn default() -> Self {
-        let old_path = std::env::var("PATH").unwrap_or_else(|_| "".to_string());
-        std::env::set_var("PATH", PROTOBUF_BIN_PATH);
+        let old_path = env::var("PATH").unwrap_or_else(|_| "".to_string());
+        env::set_var("PATH", PROTOBUF_BIN_PATH);
         Self { old_path }
     }
 }
 
 impl Drop for ScopedProtocPath {
     fn drop(&mut self) {
-        std::env::set_var("PATH", &self.old_path);
+        env::set_var("PATH", &self.old_path);
     }
+}
+
+pub enum ProtocOutputType {
+    Cc,
+    Py,
+}
+
+pub fn compile_proto(
+    import_paths: &[impl AsRef<Path>],
+    dst_path: impl AsRef<Path>,
+    file_path: impl AsRef<Path>,
+    output_types: &[ProtocOutputType],
+) {
+    let _protoc_path = ScopedProtocPath::default();
+
+    let mut args = vec![file_path.as_ref().as_os_str()];
+    args.extend(
+        vec![OsStr::new("--proto_path"); import_paths.len()]
+            .into_iter()
+            .interleave(import_paths.iter().map(|p| p.as_ref().as_os_str())),
+    );
+    args.extend(
+        output_types
+            .iter()
+            .map(|o| {
+                OsStr::new(match o {
+                    ProtocOutputType::Cc => "--cpp_out",
+                    ProtocOutputType::Py => "--python_out",
+                })
+            })
+            .interleave(vec![dst_path.as_ref().as_os_str(); output_types.len()].into_iter()),
+    );
+    let output = Command::new("protoc")
+        .args(args)
+        .output()
+        .expect("Error executing protoc – does it exist in the expected location?");
+
+    let error_msg = str::from_utf8(&output.stderr)
+        .unwrap_or("[Not valid UTF-8]")
+        .trim_end();
+    assert!(output.status.success(), "protoc complains: {}", error_msg);
 }
