e5c04d685ff9110aeda05c9fb8c83beb0bf5e50b
Nikolai Morin <nmorin@lyft.com>
2019-10-28 17:08:48 +0100

    Support geometry shader in sdl_viewer

--- sdl_viewer/src/glhelper.rs
+++ sdl_viewer/src/glhelper.rs
@@ -57,14 +57,21 @@ pub fn link_program(
     gl: &opengl::Gl,
     vertex_shader_id: GLuint,
     fragment_shader_id: GLuint,
+    geometry_shader_id: Option<GLuint>,
 ) -> GLuint {
     unsafe {
         let program = gl.CreateProgram();
         gl.AttachShader(program, vertex_shader_id);
         gl.AttachShader(program, fragment_shader_id);
+        if let Some(id) = geometry_shader_id {
+            gl.AttachShader(program, id)
+        };
         gl.LinkProgram(program);
         gl.DetachShader(program, vertex_shader_id);
         gl.DetachShader(program, fragment_shader_id);
+        if let Some(id) = geometry_shader_id {
+            gl.DetachShader(program, id)
+        };
 
         let mut status = i32::from(opengl::FALSE);
         gl.GetProgramiv(program, opengl::LINK_STATUS, &mut status);
