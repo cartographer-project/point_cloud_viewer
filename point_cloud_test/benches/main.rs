ae74bba18adbe1ea5b581323ff62cd9aba9d66c6
Marco Feuerstein <mfeuerstein@lyft.com>
2020-02-04 11:11:55 +0100

    Use stable benches. (#402)

--- point_cloud_test/benches/main.rs
+++ point_cloud_test/benches/main.rs
@@ -1,18 +1,13 @@
-#![feature(test)]
-
-extern crate test;
-
+// TODO(feuerste): Use #![feature(test)] instead of bencher once it is stable.
+use bencher::{benchmark_group, benchmark_main, black_box, Bencher};
 use point_cloud_client::PointCloudClient;
 use point_cloud_test_lib::{
     get_abb_query, get_cell_union_query, get_frustum_query, get_obb_query, make_octree,
     make_s2_cells, setup_octree_client, setup_s2_client, Arguments, SyntheticData,
 };
 use point_viewer::iterator::{PointLocation, PointQuery};
-use std::hint::black_box;
 use tempdir::TempDir;
-use test::Bencher;
 
-#[bench]
 fn bench_octree_building_multithreaded(b: &mut Bencher) {
     let mut args = Arguments::default();
     args.num_points = 100_000;
@@ -22,7 +17,6 @@ fn bench_octree_building_multithreaded(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn bench_s2_building_singlethreaded(b: &mut Bencher) {
     let mut args = Arguments::default();
     args.num_points = 100_000;
@@ -32,55 +26,62 @@ fn bench_s2_building_singlethreaded(b: &mut Bencher) {
     });
 }
 
-#[bench]
 fn all_query_octree(b: &mut Bencher) {
     run_bench(setup_octree_client, |_| PointLocation::AllPoints, b)
 }
 
-#[bench]
 fn all_query_s2(b: &mut Bencher) {
     run_bench(setup_s2_client, |_| PointLocation::AllPoints, b)
 }
 
-#[bench]
 fn box_query_octree(b: &mut Bencher) {
     run_bench(setup_octree_client, get_abb_query, b)
 }
 
-#[bench]
 fn box_query_s2(b: &mut Bencher) {
     run_bench(setup_s2_client, get_abb_query, b)
 }
 
-#[bench]
 fn frustum_query_octree(b: &mut Bencher) {
     run_bench(setup_octree_client, get_frustum_query, b)
 }
 
-#[bench]
 fn frustum_query_s2(b: &mut Bencher) {
     run_bench(setup_s2_client, get_frustum_query, b)
 }
 
-#[bench]
 fn obb_query_octree(b: &mut Bencher) {
     run_bench(setup_octree_client, get_obb_query, b)
 }
 
-#[bench]
 fn obb_query_s2(b: &mut Bencher) {
     run_bench(setup_s2_client, get_obb_query, b)
 }
 
-#[bench]
 fn cell_union_query_octree(b: &mut Bencher) {
     run_bench(setup_octree_client, get_cell_union_query, b)
 }
 
-#[bench]
 fn cell_union_query_s2(b: &mut Bencher) {
     run_bench(setup_s2_client, get_cell_union_query, b)
 }
+
+benchmark_group!(
+    benches,
+    bench_octree_building_multithreaded,
+    bench_s2_building_singlethreaded,
+    all_query_octree,
+    all_query_s2,
+    box_query_octree,
+    box_query_s2,
+    frustum_query_octree,
+    frustum_query_s2,
+    obb_query_octree,
+    obb_query_s2,
+    cell_union_query_octree,
+    cell_union_query_s2,
+);
+benchmark_main!(benches);
 
 type SetupClientFn = fn(&Arguments) -> (PointCloudClient, SyntheticData);
 
