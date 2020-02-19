5f25f5f568b64f62480255b4f82294eec2958516
Marco Feuerstein <mfeuerstein@lyft.com>
2020-02-19 19:43:16 +0100

    Reviewer comments.

--- src/utils.rs
+++ src/utils.rs
@@ -2,9 +2,10 @@ use pbr::ProgressBar;
 use std::error::Error;
 use std::io::Stdout;
 use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-pub const PROGRESS_REFRESH_RATE_SECS: u64 = 2;
+const PROGRESS_REFRESH_RATE: Duration = Duration::from_secs(2);
 
 pub fn parse_key_val<T, U>(s: &str) -> Result<(T, U), Box<dyn Error>>
 where
@@ -21,7 +22,14 @@ where
 
 pub fn create_progress_bar(total: usize, message: &str) -> ProgressBar<Stdout> {
     let mut progress_bar = ProgressBar::new(total as u64);
-    progress_bar.set_max_refresh_rate(Some(Duration::from_secs(PROGRESS_REFRESH_RATE_SECS)));
+    progress_bar.set_max_refresh_rate(Some(PROGRESS_REFRESH_RATE));
     progress_bar.message(&format!("{}: ", message));
     progress_bar
+}
+
+pub fn create_syncable_progress_bar(
+    total: usize,
+    message: &str,
+) -> Arc<Mutex<ProgressBar<Stdout>>> {
+    Arc::new(Mutex::new(create_progress_bar(total, message)))
 }
