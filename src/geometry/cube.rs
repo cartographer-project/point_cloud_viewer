cce0482fff4833d6386e4dfcdffff858c8ec6f27
Nikolai Morin <nmorin@lyft.com>
2020-03-05 13:59:54 +0100

    Forgot yet another file

---
+++ src/geometry/cube.rs
@@ -0,0 +1,57 @@
+//! An axis-aligned cube.
+
+use cgmath::{Point3, Vector3};
+use collision::{Aabb, Aabb3};
+
+#[derive(Debug, Clone)]
+pub struct Cube {
+    min: Point3<f64>,
+    edge_length: f64,
+}
+
+impl Cube {
+    pub fn bounding(aabb: &Aabb3<f64>) -> Self {
+        let edge_length = (aabb.max().x - aabb.min().x)
+            .max(aabb.max().y - aabb.min().y)
+            .max(aabb.max().z - aabb.min().z);
+        Cube {
+            min: aabb.min,
+            edge_length,
+        }
+    }
+
+    pub fn to_aabb3(&self) -> Aabb3<f64> {
+        Aabb3::new(self.min(), self.max())
+    }
+
+    pub fn new(min: Point3<f64>, edge_length: f64) -> Self {
+        Cube { min, edge_length }
+    }
+
+    pub fn edge_length(&self) -> f64 {
+        self.edge_length
+    }
+
+    pub fn min(&self) -> Point3<f64> {
+        self.min
+    }
+
+    pub fn max(&self) -> Point3<f64> {
+        Point3::new(
+            self.min.x + self.edge_length,
+            self.min.y + self.edge_length,
+            self.min.z + self.edge_length,
+        )
+    }
+
+    /// The center of the box.
+    pub fn center(&self) -> Vector3<f64> {
+        let min = self.min();
+        let max = self.max();
+        Vector3::new(
+            (min.x + max.x) / 2.,
+            (min.y + max.y) / 2.,
+            (min.z + max.z) / 2.,
+        )
+    }
+}
