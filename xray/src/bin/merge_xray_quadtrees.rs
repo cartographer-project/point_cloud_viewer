ebeb5c88cf5c86b3f0a8f7fb4bb8db6eb2902e15
Kamil Popielarz <kpopielarz@lyft.com>
2020-03-18 08:21:50 +0100

    comment

--- xray/src/bin/merge_xray_quadtrees.rs
+++ xray/src/bin/merge_xray_quadtrees.rs
@@ -3,6 +3,9 @@ use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "merge_xray_quadtrees")]
+/// Merge partial xray octrees. We assume that the root
+/// of each octree belongs to the same level of the final
+/// octree. 
 struct CommandlineArguments {
     /// Directory with partial xray quadtrees.
     #[structopt(parse(from_os_str))]
