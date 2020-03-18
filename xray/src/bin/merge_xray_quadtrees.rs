254d51613eefc8264d48024413942df445ccb01b
Kamil Popielarz <kpopielarz@lyft.com>
2020-03-18 13:30:55 +0100

    output_directory

--- xray/src/bin/merge_xray_quadtrees.rs
+++ xray/src/bin/merge_xray_quadtrees.rs
@@ -7,12 +7,17 @@ use structopt::StructOpt;
 /// of each quadtree belongs to the same level of the final
 /// quadtree.
 struct CommandlineArguments {
-    /// Directory with partial xray quadtrees.
+    /// Directory where to write the merged quadtree. Does *not*
+    /// have to be disjoint from input_directories.
+    #[structopt(parse(from_os_str), long = "output_directory")]
+    output_directory: PathBuf,
+    /// Directories with, possibly multiple, partial xray quadtrees.
     #[structopt(parse(from_os_str))]
-    directory: PathBuf,
+    input_directories: Vec<PathBuf>,
 }
 
 fn main() {
-    let _args = CommandlineArguments::from_args();
+    let args = CommandlineArguments::from_args();
+    dbg!(args);
     unimplemented!();
 }
