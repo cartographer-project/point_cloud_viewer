3a095a6654ce0d0f065e767cb6735be06fbb23c6
Kamil Popielarz <kpopielarz@lyft.com>
2020-03-18 11:40:16 +0100

    interface

---
+++ xray/src/bin/merge_xray_quadtrees.rs
@@ -0,0 +1,18 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "merge_xray_quadtrees")]
+/// Merge partial xray octrees. We assume that the root
+/// of each quadtree belongs to the same level of the final
+/// quadtree.
+struct CommandlineArguments {
+    /// Directory with partial xray quadtrees.
+    #[structopt(parse(from_os_str))]
+    directory: PathBuf,
+}
+
+fn main() {
+    let _args = CommandlineArguments::from_args();
+    unimplemented!();
+}
