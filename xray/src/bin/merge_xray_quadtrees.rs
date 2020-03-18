bd79bf1c45a7b60062461d27188c42466703687d
Kamil Popielarz <kpopielarz@lyft.com>
2020-03-18 11:34:23 +0100

    cargo fmt

--- xray/src/bin/merge_xray_quadtrees.rs
+++ xray/src/bin/merge_xray_quadtrees.rs
@@ -1,11 +1,11 @@
-use std::path::{PathBuf};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "merge_xray_quadtrees")]
 /// Merge partial xray octrees. We assume that the root
 /// of each quadtree belongs to the same level of the final
-/// quadtree. 
+/// quadtree.
 struct CommandlineArguments {
     /// Directory with partial xray quadtrees.
     #[structopt(parse(from_os_str))]
