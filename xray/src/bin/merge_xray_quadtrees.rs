e484ca16f8a3479ca5294f6d32332b1555554402
Kamil Popielarz <45685850+krzesi-mir@users.noreply.github.com>
2020-03-18 10:59:03 +0100

    Update merge_xray_quadtrees.rs

--- xray/src/bin/merge_xray_quadtrees.rs
+++ xray/src/bin/merge_xray_quadtrees.rs
@@ -4,16 +4,12 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "merge_xray_quadtrees")]
 /// Merge partial xray octrees. We assume that the root
-/// of each octree belongs to the same level of the final
-/// octree. 
+/// of each quadtree belongs to the same level of the final
+/// quadtree. 
 struct CommandlineArguments {
     /// Directory with partial xray quadtrees.
     #[structopt(parse(from_os_str))]
     directory: PathBuf,
-    /*
-    /// Paths to each quadtree's metadata protobuf file.
-    metadata_proto_filenames: Vector<Pathbuf>
-    */
 }
 
 fn main() {
