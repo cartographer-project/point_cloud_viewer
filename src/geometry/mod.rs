393851239060840f291a7ad0da9e1168c9fcd803
Nikolai Morin <nmorin@lyft.com>
2020-04-09 09:31:50 +0200

    Undo accidental change

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -3,10 +3,8 @@ mod aabb;
 mod frustum;
 mod obb;
 mod s2_cell_union;
-mod web_mercator_tile;
 
 pub use aabb::*;
 pub use frustum::*;
 pub use obb::*;
 pub use s2_cell_union::*;
-pub use web_mercator_tile::*;
