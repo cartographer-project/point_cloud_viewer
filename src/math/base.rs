7e54e1b90bf65cae3ea604ebf355c5ae1f36092f
Nikolai Morin <nmorin@lyft.com>
2020-04-09 07:56:22 +0200

    fmt

--- src/math/base.rs
+++ src/math/base.rs
@@ -39,12 +39,12 @@ impl<S: RealField + Bounded> IntersectAabb<S> for CachedAxesIntersector<S> {
 /// Use this macro as a crutch for the missing
 /// `impl<'a, S, T: ConvexPolyhedron<S>> HasAabbIntersector<'a, S> for T`.
 macro_rules! has_aabb_intersector_for_convex_polyhedron {
-	($type:ty) => (
-		impl<'a, S: RealField> HasAabbIntersector<'a, S> for $type {
-			type Intersector = CachedAxesIntersector<S>;
-		    fn aabb_intersector(&'a self) -> Self::Intersector {
-		    	self.intersector().cache_separating_axes_for_aabb()
-		    }
-		}
-	)
+    ($type:ty) => {
+        impl<'a, S: RealField> HasAabbIntersector<'a, S> for $type {
+            type Intersector = CachedAxesIntersector<S>;
+            fn aabb_intersector(&'a self) -> Self::Intersector {
+                self.intersector().cache_separating_axes_for_aabb()
+            }
+        }
+    };
 }
